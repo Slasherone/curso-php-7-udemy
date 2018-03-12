fe65041cf018cb2fcce94732578c379e8b82d1e8
Slasherone <ramon94cardoso@gmail.com>
2018-03-12 14:18:44 -0300

    Modificações realizadas em virtude da aula 64 (DAO - LIST)

--- class/Usuario.php
+++ class/Usuario.php
@@ -42,7 +42,7 @@ class Usuario {
     public function loadById($id) {
         $sql = new Sql();
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
-            ":ID" =>$id
+            ':ID' => $id
         ));
         
         if(count($results) > 0) {
@@ -56,12 +56,44 @@ class Usuario {
         }
     }
     
+    public static function getList() {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+    }
+    
+    public static function search($login) {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH' => "%{$login}%"
+        ));
+    }
+    
+    public function login($login, $password) {
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ':LOGIN' => $login,
+            ':PASSWORD' => $password
+        ));
+        
+        if(count($results) > 0) {
+            $row = $results[0];
+            
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            
+        } else {
+            throw new Exception("Login e/ou senha inválidos.");
+        }
+    }
+    
     public function __toString() {
         return json_encode(array(
-            "idusuario"=>$this->getIdusuario(),
-            "deslogin"=>$this->getDeslogin(),
-            "dessenha"=>$this->getDessenha(),
-            "dtcadastro"=>$this->getDtcadastro()->format('d/m/Y H:i:s')
+            'idusuario'=>$this->getIdusuario(),
+            'deslogin'=>$this->getDeslogin(),
+            'dessenha'=>$this->getDessenha(),
+            'dtcadastro'=>$this->getDtcadastro()->format('d/m/Y H:i:s')
         ));
     }
     
--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadById(3);
+$usuario = new Usuario();
 
-echo $root;
+//Carrega as informações de um usuario específico
+//$usuario->loadById(3);
+//echo $usuario;
+
+//Carrega uma lista com todos os usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carega uma lista de usuários buscando por um login
+//$search = Usuario::search("R");
+//echo json_encode($search);
+
+//Carrega um usuário usando login e senha
+//$usuario->login('teste', 'testando');
+//echo $usuario;
